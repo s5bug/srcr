143aa77d72183b8800113cedcf55fd8d235c4ebe
Anthony Cerruti <aprime2003@gmail.com>
2017-08-09 10:37:03 -0700

    Basic Client

--- src/srcr.cr
+++ src/srcr.cr
@@ -1,5 +1,8 @@
 require "./srcr/*"
+require "http/client"
 
 module SRcr
-
+  USER_AGENT = "SRcr/" + SRcr::VERSION
+  API_ROOT = "https://www.speedrun.com/api/v1/"
+  CLIENT = HTTP::Client.new(URI.parse API_ROOT)
 end
