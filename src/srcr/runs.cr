44b96a4fe6da0838c13c537e2cb710a210419245
Anthony Cerruti <aprime2003@gmail.com>
2017-08-11 10:51:09 -0700

    more mappings

--- src/srcr/runs.cr
+++ src/srcr/runs.cr
@@ -12,7 +12,10 @@ module SRcr
       category: {type: String, setter: false},
       videos: {type: SRcr::VideoListing, setter: false},
       status: {type: SRcr::RunStatus, setter: false},
-      players: {type: Array(SRcr::Player), setter: false}
+      players: {type: Array(SRcr::Player), setter: false},
+      date: {type: Time, nilable: true, converter: Time::Format.new("%Y-%m-%d"), setter: false},
+      submitted: {type: Time, nilable: true, converter: Time::Format.new("%Y-%m-%d"), setter: false},
+      times: {type: SRcr::TimeSet, setter: false}
     )
   end
   class VideoListing
@@ -32,5 +35,32 @@ module SRcr
   enum PlayerType
     User
     Guest
+  end
+  class TimeSet
+    JSON.mapping(
+      primary: {type: Time, converter: SRcr::TimeSetStringConverter, setter: false}
+    )
+  end
+  class TimeSetStringConverter
+
+    TIME_REGEX = /PT(?:(?<h>\d+)H)?(?:(?<m>\d+)M)?(?<s>\d+(?:\.\d+)?)S/
+
+    def self.from_json(value : JSON::PullParser) : Time
+      d = TIME_REGEX.match(value.read_string)
+      t = 0.days
+      if(!d["h"]?.is_nil?)
+        t += d["h"].to_i.hours
+      end
+      if(!d["m"]?.is_nil?)
+        t += d["m"].to_i.minutes
+      end
+      if(!d["s"]?.is_nil?)
+        t += d["s"].to_f.seconds
+      end
+      t
+    end
+    def self.to_json(value : Time, json : JSON::Builder)
+      "null" # TODO Implement a thing
+    end
   end
 end
